e9200964ece9b2d4546d911281a12029cc106811
DudarIg <dudar.ig@yandex.ru>
2022-06-23 08:41:00 +0300

    2 method1

--- app/src/main/java/ru/pakad/pakadcripto/api/ApiService.kt
+++ app/src/main/java/ru/pakad/pakadcripto/api/ApiService.kt
@@ -7,17 +7,22 @@ import retrofit2.http.Query
 import ru.pakad.pakadcripto.pojo.CoinPriceInfoRawData
 
 interface ApiService {
+
     @GET("top/totalvolfull")
     fun getTopCoinsInfo(
-        @Query("api_key") api_Key: String = "1277c72af4a1a94164f3a0e4e68c7ab82067fd654f410a884e7e5ceeba391118",
+        @Query("api_key") api_Key: String = API_KEY,
         @Query("limit") limit: Int = 10,
         @Query("tsym") tSym : String ="USD"
     ): Single<CointInfoListOfData>
 
     @GET("pricemultifull")
     fun getFullPriceList(
-        @Query("api_key") api_Key: String = "1277c72af4a1a94164f3a0e4e68c7ab82067fd654f410a884e7e5ceeba391118",
+        @Query("api_key") api_Key: String = API_KEY,
         @Query("fsyms") fSyms: String,
         @Query("tsyms") tSyms: String = "USD"
     ): Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val API_KEY = "1277c72af4a1a94164f3a0e4e68c7ab82067fd654f410a884e7e5ceeba391118"
+    }
 }
